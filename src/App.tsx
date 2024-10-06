ad0c3a9cda51751d090b595af269215127f78fe2
jamesgiu <james.giuffrida@gmail.com>
2024-10-06 21:49:30 +1100

    QK-01 bg and other bits

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,40 @@
-import React from 'react';
 import '@elastic/eui/dist/eui_theme_dark.css';
 import "./index.scss";
 import "./index.css";
+import "./App.css";
 
-import { EuiLoadingLogo, EuiProvider, EuiText, EuiEmptyPrompt, EuiButton, EuiTitle, EuiLink, EuiThemeProvider, EuiCode, EuiThemeAmsterdam } from '@elastic/eui';
-import { ThemeProvider } from '@emotion/react';
+import { EuiLoadingLogo, EuiProvider, EuiEmptyPrompt, EuiButton, EuiLink, EuiFlexGroup, EuiCode, EuiFlexItem, EuiPanel, EuiText, EuiTitle, EuiPageHeader } from '@elastic/eui';
 
 const MyApp = () => (
   <EuiProvider>
-    <EuiEmptyPrompt
-    icon={<EuiLoadingLogo logo="cloudSunny" size="xl" />}
-    title={<h2>Do you need a jacket today, Melbourne?</h2>}
-    body={<p>Find out from a trusted source immediately.</p>}
-    actions={
-      <EuiButton color="primary" fill>
-        Find out
-      </EuiButton>
-    }
-    footer={
-      <>
-        <EuiTitle size="xxs">
-          <h3>Want to learn more?</h3>
-        </EuiTitle>
-        <EuiLink href="#" target="_blank">
-          Read the docs
-        </EuiLink>
-      </>
-    }
-  />
+    <div className="app-content">
+      <div className="bg-image-wrapper"/>
+      <EuiEmptyPrompt
+      icon={<EuiLoadingLogo logo="cloudSunny" size="xl" />}
+      title={<h2>Do you need a jacket today, Melbourne?</h2>}
+      footer={
+        <>
+          <EuiLink href="#" target="https://github.com/jamesgiu/quick-fit">
+          © 2024 Jiv Pty Ltd jamesgiu/quick-fit
+          </EuiLink>
+        </>
+      }
+    />
+      <EuiFlexGroup>
+  
+    <EuiFlexItem>
+      <EuiPanel>
+       <EuiPageHeader pageTitle="Brunching..." description="Do you need a jacket out and about in Melbourne today?"/>
+      </EuiPanel>
+    </EuiFlexItem>
+
+    <EuiFlexItem>
+      <EuiPanel grow={false}>
+      <EuiPageHeader pageTitle="Kick-ons..." description="Will you need a jacket for tonight?"/>
+      </EuiPanel>
+    </EuiFlexItem>
+  </EuiFlexGroup>
+    </div>
   </EuiProvider>
 );
 
