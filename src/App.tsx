18ddadd0a862d762fc443277737583521c1b5cd0
jamesgiu <james.giuffrida@gmail.com>
2024-10-07 22:18:51 +1100

    QF-01 extra style tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -3,37 +3,49 @@ import "./index.scss";
 import "./index.css";
 import "./App.css";
 
-import { EuiLoadingLogo, EuiProvider, EuiEmptyPrompt, EuiButton, EuiLink, EuiFlexGroup, EuiCode, EuiFlexItem, EuiPanel, EuiText, EuiTitle, EuiPageHeader } from '@elastic/eui';
+import { EuiLoadingLogo, EuiProvider, EuiEmptyPrompt, EuiLink, EuiFlexGroup, EuiFlexItem, EuiPanel, EuiPageHeader, EuiCard, EuiIcon, EuiLoadingChart } from '@elastic/eui';
 
 const MyApp = () => (
   <EuiProvider>
     <div className="app-content">
       <div className="bg-image-wrapper"/>
       <EuiEmptyPrompt
-      icon={<EuiLoadingLogo logo="cloudSunny" size="xl" />}
+      icon={   <EuiLoadingChart size="xl" />}
       title={<h2>Do you need a jacket today, Melbourne?</h2>}
       footer={
-        <>
+        <div className="qf-footer">
           <EuiLink href="#" target="https://github.com/jamesgiu/quick-fit">
           © 2024 Jiv Pty Ltd jamesgiu/quick-fit
           </EuiLink>
-        </>
+        </div>
       }
-    />
-      <EuiFlexGroup>
-  
-    <EuiFlexItem>
-      <EuiPanel>
-       <EuiPageHeader pageTitle="Brunching..." description="Do you need a jacket out and about in Melbourne today?"/>
-      </EuiPanel>
-    </EuiFlexItem>
-
-    <EuiFlexItem>
-      <EuiPanel grow={false}>
-      <EuiPageHeader pageTitle="Kick-ons..." description="Will you need a jacket for tonight?"/>
-      </EuiPanel>
-    </EuiFlexItem>
-  </EuiFlexGroup>
+      />
+      <EuiFlexGroup>  
+        <EuiFlexItem>
+          <EuiCard
+          className="day-card"
+          icon={<EuiLoadingLogo size="xl" logo={"sun"}/>}
+          title={"Brunching..."}
+          description="Do you need a jacket out and about in Melbourne today?"
+          betaBadgeProps={{
+            label: 'Brunching...',
+            color: 'accent',
+          }}
+        />
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiCard
+          className="night-card"
+          icon={<EuiLoadingLogo size="xl" logo={"moon"}/>}
+          title={"Kick-ons..."}
+          description="Will you need a jacket for tonight?"
+          betaBadgeProps={{
+            label: 'Kick-ons...',
+            color: 'subdued',
+          }}
+        />
+        </EuiFlexItem>
+      </EuiFlexGroup>
     </div>
   </EuiProvider>
 );
