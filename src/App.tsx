578dbdc4d4f91f8efe72b77bf3b9b0873012a118
jamesgiu <james.giuffrida@gmail.com>
2024-10-09 00:04:49 +1100

    QF-01 clean up + weather in correct boxes

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,63 @@ import "./index.scss";
 import "./index.css";
 import "./App.css";
 
-import { EuiLoadingLogo, EuiProvider, EuiEmptyPrompt, EuiLink, EuiFlexGroup, EuiFlexItem, EuiCard, EuiTitle, EuiIcon } from '@elastic/eui';
+import { EuiLoadingLogo, EuiProvider, EuiEmptyPrompt, EuiLink, EuiFlexGroup, EuiFlexItem, EuiCard, EuiTitle, EuiIcon, EuiStat, EuiPanel, EuiTextColor } from '@elastic/eui';
 import { useEffect, useState } from 'react';
 import { getWeather, WeatherType } from './MeteoApi';
-import { Chart, Metric, Settings } from '@elastic/charts';
 
 const MyApp = () => {
 
   const [weatherData, setWeatherData] = useState<WeatherType>();
+  const [hourMap, setHourMap] = useState<Map<Date, number>>();
   const [isDay, setIsDay] = useState<boolean>();
+  const [soonAverageTemp, setSoonAverageTemp] = useState<number>();
+
+  const getSoonAverageTemp = (): number => {
+    if (weatherData) {
+      return Math.round((weatherData?.hourly.temperature2m[0] + weatherData?.hourly.temperature2m[1] + weatherData?.hourly.temperature2m[2] + weatherData?.hourly.temperature2m[3] + weatherData?.hourly.temperature2m[4] + weatherData?.hourly.temperature2m[5]) / 6);;
+    }
+
+    return 0;
+  }
+
+  const getTonightTemp = (): number => {
+    if (weatherData) {
+      let tonightAvg = 0;
+      // Filter to be only indices that are for today, and 6pm or later.
+      const tonightDates = weatherData.hourly.time.filter(time => time.getDay() === new Date(Date.now()).getDay() && time.getHours() >= 18);
+      tonightDates.forEach((date) => {
+        tonightAvg += hourMap?.get(date)!;
+      });
+
+      tonightAvg = tonightAvg / tonightDates.length;
+      return Math.round(tonightAvg);
+    }
+
+    return 0;
+  }
+
+  const getTomorrowTemp = (): number => {
+    if (weatherData) {
+      let tomorrowAvg = 0;
+      // Filter to be only indices that are for tomorrow, and before 6pm.
+      const tomorrowDates = weatherData.hourly.time.filter(time => time.getDay() !== new Date(Date.now()).getDay() && time.getHours() > 7 && time.getHours() < 18);
+      tomorrowDates.forEach((date) => {
+        tomorrowAvg += hourMap?.get(date)!;
+      });
+
+      tomorrowAvg = tomorrowAvg / tomorrowDates.length;
+      return Math.round(tomorrowAvg);
+    }
+
+    return 0;
+  }
 
   const dayCard =           <EuiFlexItem>
   <EuiCard
   className="day-card"
   icon={<EuiLoadingLogo size="xl" logo={"sun"}/>}
-  title={"Brunching..."}
-  description="Do you need a jacket today?"
+  title={`${soonAverageTemp}°C`}
+  description={isDay && weatherData && `Today's coldest is ${Math.round(weatherData!.daily.apparentTemperatureMin[0])}°C, and the warmest is ${Math.round(weatherData!.daily.apparentTemperatureMax[0])}°C`}
   betaBadgeProps={{
     label: 'Today',
     color: 'accent',
@@ -30,8 +71,8 @@ const tomorrowCard =           <EuiFlexItem>
 <EuiCard
 className="day-card tomorrow-card"
 icon={<EuiLoadingLogo size="xl" logo={"sun"}/>}
-title={"Brunching..."}
-description="Will you need a jacket tomorrow?"
+title={`${getTomorrowTemp()}°C`}
+description="8am - 5pm"
 betaBadgeProps={{
   label: 'Tomorrow',
   color: 'accent',
@@ -43,8 +84,8 @@ const nightCard =           <EuiFlexItem>
 <EuiCard
 className="night-card"
 icon={<EuiLoadingLogo size="xl" logo={"moon"}/>}
-title={"Kick-ons..."}
-description="Will you need a jacket for tonight?"
+title={isDay ? `${getTonightTemp()}°C` : `${soonAverageTemp}°C`}
+description= {isDay && weatherData ? `The minimum temperature will feel like ${Math.round(weatherData?.daily.apparentTemperatureMin[0])}°C` : weatherData &&  `Today's coldest felt like ${Math.round(weatherData!.daily.apparentTemperatureMin[0])}°C, and the warmest felt like  ${Math.round(weatherData!.daily.apparentTemperatureMax[0])}°C`}
 betaBadgeProps={{
   label: 'Tonight',
   color: 'subdued',
@@ -77,8 +118,21 @@ const currentTemp = () => {
   }, []);
 
   useEffect(()=> {
-    setIsDay(weatherData?.current.isDay === 1);
+    setIsDay(weatherData?.current.isDay === 1)
+    setSoonAverageTemp(getSoonAverageTemp());
+    
+    const hoursMap = new Map<Date, number>();
+
+    if (weatherData) {
+      for (let i=0; i < weatherData?.hourly.time.length; i++) {
+        hoursMap.set(weatherData.hourly.time[i], weatherData.hourly.temperature2m[i]);
+      }  
+
+      setHourMap(hoursMap);
+    }
+
   }, [weatherData])
+
   
   // TODO
   // function to return 'card' with temp, icon and yes/no for jacket depending on passed CURRENT.
@@ -89,7 +143,7 @@ const currentTemp = () => {
   return (
     <EuiProvider>
       <div className="app-content">
-        <div className="bg-image-wrapper"/>
+        <div className={`bg-image-wrapper ${isDay ? 'bg-day' : 'bg-night'}`}/>
         <EuiEmptyPrompt
         icon={ <div className="app-logo"/>}
         title={<EuiTitle className="logo-subtext" size='l'><h1>QuickFit</h1></EuiTitle>}
@@ -105,7 +159,7 @@ const currentTemp = () => {
         }
         />
         {currentTemp()}
-        <EuiFlexGroup>  
+        <EuiFlexGroup className='cards-flex-group'>  
           {isDay ? <>{dayCard}{nightCard}</> : <>{nightCard}{tomorrowCard}</> }
         </EuiFlexGroup>
       </div>
